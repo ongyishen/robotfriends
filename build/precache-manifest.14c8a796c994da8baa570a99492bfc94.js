8cd2934fe84c565d563caffab5950bc775b2901d
ONG YI SHEN <ongyishen@yahoo.com>
2021-05-16 16:08:25 +0800

    publish

---
+++ build/precache-manifest.14c8a796c994da8baa570a99492bfc94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7efabb5cede7c7636155",
+    "url": "/robotfriends/static/css/main.349b0e0f.chunk.css"
+  },
+  {
+    "revision": "7efabb5cede7c7636155",
+    "url": "/robotfriends/static/js/main.2fc6e3f2.chunk.js"
+  },
+  {
+    "revision": "aebb4e0e6c2039467182",
+    "url": "/robotfriends/static/js/runtime~main.6fe61ee8.js"
+  },
+  {
+    "revision": "07d669626425603c04d5",
+    "url": "/robotfriends/static/css/2.aa4731e4.chunk.css"
+  },
+  {
+    "revision": "07d669626425603c04d5",
+    "url": "/robotfriends/static/js/2.8cb79892.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c4cfb2317a9d5f61908e0a63a7677a47",
+    "url": "/robotfriends/index.html"
+  }
+];
